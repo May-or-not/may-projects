b08fce41a4295be5ff98407a57f3a2d99489ab7e
May-or-not <jose.may@udem.edu>
2022-10-11 23:30:38 -0500

    Pruebas con el código

--- Python/test.py
+++ Python/test.py
@@ -1,7 +1,12 @@
-"Hola mundo"
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 
 #Este código realiza la impresión de tu primer hola mundo
 print("Hola Mundo")
+
+x = np.linspace(10, 20, 50)
+y = np.linspace(90, 100, 50)
+
+plt.figure(1, edgecolor='r')
+plt.plot(x,y, color = 'k')
+plt.show()
